5f0f613aa4c54724f76739497a20fd9b20c2dd3f
Pal Bijewar <pal.bijewar@gmail.com>
2023-04-15 14:54:09 +0530

    script js changes

--- script.js
+++ script.js
@@ -60,7 +60,6 @@ audio.pause();
             aniDur = parseFloat(window.getComputedStyle(tom, null).getPropertyValue('animation-duration'));
             newDur = aniDur - 0.1;
             tom.style.animationDuration = newDur + 's';
-            console.log('New animation duration: ', newDur)
         }, 500);
       }
  
